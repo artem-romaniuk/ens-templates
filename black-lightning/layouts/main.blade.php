d0f5dae6533007ea38f8bd633f1717707ed89735
IEugeneI <eugenelboichenko@gmail.com>
2024-05-21 16:43:11 +0300

    fix "Black Lightning" template

--- black-lightning/layouts/main.blade.php
+++ black-lightning/layouts/main.blade.php
@@ -191,12 +191,12 @@
 
 <div id="content">
     <div style="
-    @if( empty(settings('logo_settings.header.height')) || settings('logo_settings.header.height') == 'auto') padding-top: '0%'; @endif
-    @if(  settings('logo_settings.header.height') == 50) padding-top: '0%'; @endif
-    @if(  settings('logo_settings.header.height') == 100) padding-top: '3.5%'; @endif
-    @if(  settings('logo_settings.header.height') == 150) padding-top: '6%'; @endif
-    @if(  settings('logo_settings.header.height') == 200) padding-top: '8%'; @endif
-    @if(  settings('logo_settings.header.height') == 250) padding-top: '10%'; @endif
+        @if( empty(settings('logo_settings.header.height')) || settings('logo_settings.header.height') == 'auto') padding-top: 0; @endif
+        @if(  settings('logo_settings.header.height') == 50) padding-top: 0; @endif
+        @if(  settings('logo_settings.header.height') == 100) padding-top: 3.5%; @endif
+        @if(  settings('logo_settings.header.height') == 150) padding-top: 6%; @endif
+        @if(  settings('logo_settings.header.height') == 200) padding-top: 8%; @endif
+        @if(  settings('logo_settings.header.height') == 250) padding-top: 10%; @endif
     ">
         @yield('content')
     </div>
