d9f057df2b48b360bd55d995d4d909feb7a697d4
artur_pandateam <artur.koliada@pandateam.net>
2024-05-22 19:07:22 +0300

    [SNTSTSS-342] Shadow: removed padding from #content

--- shadow/layouts/main.blade.php
+++ shadow/layouts/main.blade.php
@@ -80,16 +80,7 @@
 <div class="back-to-top"></div>
 
 <div id="content">
-    <div style="
-        @if(empty(settings('logo_settings.header.height')) || settings('logo_settings.header.height') == 'auto') padding-top: 0; @endif
-        @if(settings('logo_settings.header.height') == 50) padding-top: 3%; @endif
-        @if(settings('logo_settings.header.height') == 100) padding-top: 3.5%; @endif
-        @if(settings('logo_settings.header.height') == 150) padding-top: 6%; @endif
-        @if(settings('logo_settings.header.height') == 200) padding-top: 8%; @endif
-        @if(settings('logo_settings.header.height') == 250) padding-top: 11.5%; @endif
-    ">
-        @yield('content')
-    </div>
+@yield('content')
 </div>
 
 <footer class="page-footer bg-image" style="background-image: url(../assets/img/world_pattern.svg);">
