72e96c687168cc4af332cc700d08b2338165b0f3
Artem Romaniuk <artem.romaniuk@gmail.com>
2024-06-07 17:27:48 +0300

    Added scroll-bar to navbar to Shadow theme

---
+++ shadow/layouts/includes/navbarStyles.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .navbar-collapse {
+        overflow-x: auto;
+        padding-bottom: 8px;
+        /*scrollbar-width: thin;*/
+        /*scrollbar-color: #a7a7a7 #f1f1f1;*/
+    }
+
+    .navbar-collapse::-webkit-scrollbar {
+        width: 1px;
+        height: 1px;
+    }
+
+    .navbar-collapse::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+
+    .navbar-collapse::-webkit-scrollbar-thumb {
+        background: #cecece;
+    }
+
+    .navbar-collapse::-webkit-scrollbar-thumb:hover {
+        background: #a7a7a7;
+    }
+</style>
